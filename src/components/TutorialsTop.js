f4535d41e050067ed10ef688229fd04e00c15e1f
Giorgio Angelotti <76100950+giorgioangel@users.noreply.github.com>
2024-03-04 08:50:53 +0100

    Update src/components/TutorialsTop.js
    Co-authored-by: danieldegroot2 <67521919+danieldegroot2@users.noreply.github.com>

--- src/components/TutorialsTop.js
+++ src/components/TutorialsTop.js
@@ -32,6 +32,10 @@ export function TutorialsTop({ highlightId } = {}) {
                 </video>
                 <div className="text-sm">Ink Detection</div>
             </a>
+            <div className="hidden sm:flex mx-2 mb-2 flex-col items-center">
+                <div className="relative leading-[150px] py-4 w-[16px] text-center">→</div>
+                <div className="text-sm">&nbsp;</div>
+            </div>
             <a href="/segmentation_tutorial" className={`mb-2 flex flex-col items-center w-[100px] sm:w-[150px] relative box-content p-2 sm:p-4 sm:pb-2 hover:bg-[#fefefe26] rounded-xl ${ highlightId == 5 ? "bg-[#fffefc30] hover:bg-[#fefefe45]" : "" }`}>
             <img className="max-h-[300px] m-2" src="/img/tutorials/fisherman.webp"/>
             <div className="text-sm">Segmentation: Intuitive Tutorial</div>
